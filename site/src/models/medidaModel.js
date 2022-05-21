498206c9c8f4a2eda61b5b02cabcedc10724bfe6
Mateus Pereira da Silva <mateus.silva@sptech.school>
2022-05-21 09:49:09 -0300

    Melhorando conforme os ajustes feitos na api da Aquatec

--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -1,13 +1,13 @@
 var database = require("../database/config");
 
 function buscarUltimasMedidas(idAquario, limite_linhas) {
-    
+
     instrucaoSql = ''
-    
+
     if (process.env.AMBIENTE_PROCESSO == "producao") {
         instrucaoSql = `select top ${limite_linhas}
-                        temperatura, 
-                        umidade, 
+        dht11_temperatura as temperatura, 
+        dht11_umidade as umidade,  
                         momento,
                         CONVERT(varchar, momento, 108) as momento_grafico
                     from medida
@@ -15,8 +15,8 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
                     order by id desc`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `select 
-                        temperatura, 
-                        umidade, 
+        dht11_temperatura as temperatura, 
+        dht11_umidade as umidade,
                         momento,
                         DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
                     from medida
@@ -26,27 +26,29 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
     }
-    
+
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 function buscarMedidasEmTempoReal(idAquario) {
-    
+
     instrucaoSql = ''
-    
-    if (process.env.AMBIENTE_PROCESSO == "producao") {       
+
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
         instrucaoSql = `select top 1
-                        temperatura, 
-                        umidade, CONVERT(varchar, momento, 108) as momento_grafico, 
+        dht11_temperatura as temperatura, 
+        dht11_umidade as umidade,  
+                        CONVERT(varchar, momento, 108) as momento_grafico, 
                         fk_aquario 
                         from medida where fk_aquario = ${idAquario} 
                     order by id desc`;
-        
+
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `select 
-                        temperatura, 
-                        umidade, DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
+        dht11_temperatura as temperatura, 
+        dht11_umidade as umidade,
+                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
                         fk_aquario 
                         from medida where fk_aquario = ${idAquario} 
                     order by id desc limit 1`;
@@ -64,3 +66,4 @@ module.exports = {
     buscarUltimasMedidas,
     buscarMedidasEmTempoReal
 }
+
