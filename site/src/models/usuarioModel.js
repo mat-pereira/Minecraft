d2ece65248ef7fcaedb593e5441bd5510e1c2e06
Mateus <Mateus.silva@sptech.school>
2022-05-26 13:35:15 -0300

    Comentário ajuda prof Fernanda

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -7,7 +7,7 @@ function listar() {
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
-}
+} // Criar uma dessa soq com contar usuarios
 
 function entrar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
--- site/src/routes/usuarios.js
+++ site/src/routes/usuarios.js
@@ -9,7 +9,7 @@ router.get("/", function (req, res) {
 
 router.get("/listar", function (req, res) {
     usuarioController.listar(req, res);
-});
+}); // Fazer igual a rounter.get /contartodos -> ir para controllers
 
 //Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
 router.post("/cadastrar", function (req, res) {
