044a3ad392840915b1157bc029d22e3b411cafd8
Mateus Pereira <mateus.silva@sptech.school>
2022-06-01 14:04:01 -0300

    Tirando contagem de usuarios

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -9,14 +9,7 @@ function listar() {
     return database.executar(instrucao);
 } 
 
-function ObterContagemUsuariosCadastrados() {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
-    var instrucao = `
-        SELECT COUNT(id) FROM pm_usuarios;
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
-} 
+
 
 function entrar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
@@ -44,5 +37,4 @@ module.exports = {
     entrar,
     cadastrar,
     listar,
-    ObterContagemUsuariosCadastrados,
 };
